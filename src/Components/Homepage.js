834e659f636473a6103c9530b61877f0b1548b07
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-12-11 19:53:08 +0530

    Festival Timeline

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -31,22 +31,46 @@ class Homepage extends React.Component {
 
 
                 <div className="even-section padding">
+                    <div className="section-title">
+                        <h1>Festivals</h1>
+                    </div>
+                <div className="Timeline">
                     <Container>
-                        <h1>Major Festivals</h1>
-                        <Row>
-                            <Card className="col-4">
-                                <Card.Img variant="top" src={require('../img/jumboImg.jpg')} />
-                                <Card.Body>
-                                    <Card.Title>Subtitle</Card.Title>
-                                    <Card.Text>
-                                        Some info
-                                    </Card.Text>
-                                    <Button variant="primary" href="tourist-spot/1">Check Out</Button>
-                                </Card.Body>
-                            </Card>
-                        </Row>
-                        <a href="/tourist-spots"><p>More</p></a>
+                        <div className="timeline-item-left">
+                            <div className="timeline-img">
+                                
+                            </div>
+                            <div className="timeline-content">
+                                <div className="date">1 Jan 2020</div>
+                                <img src={require('../img/jumboImg.jpg')} className="act-img" alt="..."/>
+                                <h3>Title</h3>
+                                <h4>Subtitle</h4>
+                            </div>
+                        </div>
+                        <div className="timeline-item-right">
+                            <div className="timeline-img">
+                                
+                            </div>
+                            <div className="timeline-content">
+                                <div className="date">1 Jan 2020</div>
+                                <img src={require('../img/jumboImg.jpg')} className="act-img" alt="..."/>
+                                <h3>Title</h3>
+                                <h4>Subtitle</h4>
+                            </div>
+                        </div>
+                        <div className="timeline-item-left">
+                            <div className="timeline-img">
+                                
+                            </div>
+                            <div className="timeline-content">
+                                <div className="date">1 Jan 2020</div>
+                                <img src={require('../img/jumboImg.jpg')} className="act-img" alt="..."/>
+                                <h3>Title</h3>
+                                <h4>Subtitle</h4>
+                            </div>
+                        </div>
                     </Container>
+            </div>
                 </div>
 
                 <div className="odd-section padding">
