e9e6b59d31ea63236238eb46ce41e6736ebfea3f
sss9700 <50237152+sss9700@users.noreply.github.com>
2020-12-12 20:57:30 +0400

    Update thingstodo.js

--- src/Constants/thingstodo.js
+++ src/Constants/thingstodo.js
@@ -29,6 +29,12 @@ const thingstodo = [
         imgLink: require('../img/things_to_do/brunch_at_green_hotel.jpg'),
         description: "People come here for lunch and to just hang and meet friends in the gardens. They have farmers markets and vendor booths set up. A great place to spend the afternoon and the food here is great too. ",
     }
+    {
+        id: 5,
+        name: "Explore Yoga",
+        imgLink: require('../img/things_to_do/yoga.jpg'),
+        description: "This world-famous centre for yoga attracts thousands of international students each year to learn, practise or become certified in teaching Ashtanga. Indeed, the city's connection with yoga is so profound that it is linked with a practice of yoga, Mysore Style, that's recognised around the world.",
+    },
 ]
 
 export default thingstodo;
